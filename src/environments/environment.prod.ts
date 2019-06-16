e481ae38937d4227c7aebcf15aff9a75963be47a
Jaydev Thomke <jaydev.thomke@firstcry.com>
2019-06-16 15:11:00 +0530

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+/*
+ * spurtcommerce
+ * version 2.1
+ * http://www.spurtcommerce.com
+ *
+ * Copyright (c) 2019 piccosoft ltd
+ * Author piccosoft ltd <support@piccosoft.com>
+ * Licensed under the MIT license.
+ */
+export const environment = {
+  production: true,
+  storeUrl:  'https://pune-local-store.herokuapp.com/api',
+  imageUrl: 'https://pune-local-store.herokuapp.com/api/media/image-resize'
+};
