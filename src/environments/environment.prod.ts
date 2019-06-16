08802fc50e1d8e9b91143e6a71f490b9c9f49624
Jaydev Thomke <jaydev.thomke@firstcry.com>
2019-06-16 19:02:26 +0530

    api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,6 +9,6 @@
  */
 export const environment = {
   production: true,
-  storeUrl:  'https://pune-local-store.herokuapp.com/api',
+  storeUrl:  'https://pune-local-store.herokuapp.com/api/',
   imageUrl: 'https://pune-local-store.herokuapp.com/api/media/image-resize'
 };
